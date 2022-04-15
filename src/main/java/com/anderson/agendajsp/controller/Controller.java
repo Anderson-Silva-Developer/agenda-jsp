493b8698515f8db3e1456772c16e4807567f71df
asc <silvaandersonpi@gmail.com>
2022-04-15 14:55:01 -0300

    add DAO

--- src/main/java/com/anderson/agendajsp/controller/Controller.java
+++ src/main/java/com/anderson/agendajsp/controller/Controller.java
@@ -1,15 +1,26 @@
 package com.anderson.agendajsp.controller;
 
+import com.anderson.agendajsp.model.DAO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 
 @WebServlet(name = "Controller", value = "/main")
 public class Controller extends HttpServlet {
+    DAO dao=new DAO();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        response.setContentType("text/html");
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>"+"main"+"</h1>");
+        out.println("</body></html>");
     }
 
 }
--- src/main/java/com/anderson/agendajsp/model/DAO.java
+++ src/main/java/com/anderson/agendajsp/model/DAO.java
@@ -1,4 +1,25 @@
 package com.anderson.agendajsp.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class DAO {
-}
+
+    private  String driver="org.postgresql.Driver";
+    private  String url = "jdbc:postgresql:/localhost:5432/dbagenda?useTimezone=UTC";
+    private  String user=System.getenv("USER");
+    private  String password=System.getenv("PASSWORD");;
+
+    private Connection conectar(){
+        Connection conn=null;
+        try {
+         Class.forName(driver);
+         conn= DriverManager.getConnection(url,user,password);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+
+    }
+  }
